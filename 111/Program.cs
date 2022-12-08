9db2fc4609d5ce01c5ef205e3ff029e4ad069503
Artem <118046594+Artem160188@users.noreply.github.com>
2022-12-08 12:38:30 +0300

    Add files via upload

---
+++ 111/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, которая покажет количество 
+// чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+// int[] FillPrintArray() 
+
+        // int[] array = new int[30]; 
+        // for (int i = 0; i < 30; i++) 
+        // { 
+        //     array[i] = new Random().Next(100, 999); 
+        //     Console.Write(array[i] + " "); 
+        // } 
+int[] FillPrintArray() 
+{ 
+    { 
+        int[] array = new int[30]; 
+        for (int i = 0; i < 30; i++) 
+        { 
+            array[i] = new Random().Next(100, 1000); 
+            Console.Write(array[i] + " "); 
+        } 
+        return array; 
+    } 
+} 
+ 
+int Elements(int[] array) 
+{ 
+    int count = 0; 
+    foreach (int el in array) 
+    { 
+        //if (el >= 100 && el <=500) count++; 
+        if (el%2==0) count++; 
+    } 
+    return count; 
+} 
+ 
+int[] array = FillPrintArray(); 
+Console.WriteLine(); 
+Console.WriteLine($"количество чётных чисел в массиве = {Elements(array)}");
---
+++ 333/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными
+//  числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//{
+var rnd = new Random();
+var arr = new int[15];
+for (int i = 0; i < arr.Length; i++)
+    Console.Write("{0}\t", arr[i] = rnd.Next(100, 1000));
+Console.WriteLine();
+var odd = arr.Where((element, index) => index % 2 == 0);
+Console.WriteLine("Нечётные. Количество: {0}. Сумма: {1}.", odd.Count(), odd.Sum());
+
+//Console.ReadKey();
+//}
